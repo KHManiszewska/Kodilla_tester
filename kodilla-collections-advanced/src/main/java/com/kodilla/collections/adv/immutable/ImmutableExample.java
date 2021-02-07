62c393a3701815d04f45c1b4a62dcb86cec5ee0e
Katarzyna Maniszewska <k.maniszewska@icloud.com>
2021-02-07 14:50:22 +0100

    BookManager

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/ImmutableExample.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/ImmutableExample.java
@@ -2,6 +2,10 @@ package com.kodilla.collections.adv.immutable;
 
 public class ImmutableExample {
     public static void main(String[] args) {
-
+        Book book = new BookHacked("John Stewart", "The last chance");
+        //book.title = "New";
+        BookHacked bookHacked = (BookHacked) book;
+        bookHacked.modifyTitle("New title");
+        System.out.println(book.getTitle());
     }
 }
